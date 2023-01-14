d9e3c004ac37d5fb673f204640f2731a7b5df7f9
Helix <hissyu@hotmail.com>
2023-01-14 10:12:12 +0800

    add missing vm class, but this is not perfect

---
+++ NCloudMusic3/ViewModels/UserStateViewModel.cs
@@ -0,0 +1,26 @@
+﻿using NCloudMusic3.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NCloudMusic3.ViewModels
+{
+    public class ViewModel<T> : NotifyPropertyChanged where T : new()
+    {
+        protected T Model { get; set; } = new();
+
+        public void SetModel(T model)
+        {
+            Model = model;
+            foreach (var p in this.GetType().GetProperties())
+            {
+                RaisePropertyChanged(p.Name);
+            }
+        }
+    }
+
+}
